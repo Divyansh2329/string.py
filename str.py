26dbe0851cbc148fb0e9dbe34a93c5b782dbc607
Divyansh2329 <73928730+Divyansh2329@users.noreply.github.com>
2021-01-28 05:27:41 +0530

    Create str.py

---
+++ str.py
@@ -0,0 +1,15 @@
+#Values assigned to variable x :
+x = "MY NAME IS DIVYANSH DIXIT"
+
+#display x:
+print(x)
+y: str = "hello"
+z: str = "world"
+r = y & z
+print(r)
+#Multiple strings
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
