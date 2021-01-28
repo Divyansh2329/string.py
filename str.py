c88a1d9ceb75ced56b8aa66798f7e20afd6b99c1
Divyansh2329 <73928730+Divyansh2329@users.noreply.github.com>
2021-01-28 05:47:13 +0530

    Update str.py

--- str.py
+++ str.py
@@ -3,13 +3,28 @@ x = "MY NAME IS DIVYANSH DIXIT"
 
 #display x:
 print(x)
+#using operators:
 y: str = "hello"
 z: str = "world"
-r = y & z
+r: str = y + z
 print(r)
-#Multiple strings
+#Multiple strings:
 a = """Lorem ipsum dolor sit amet,
 consectetur adipiscing elit,
 sed do eiusmod tempor incididunt
 ut labore et dolore magna aliqua."""
 print(a)
+#string array
+b = 'I am an idiot'
+print(b[6])
+var: str = 'yooo'
+bar : str = "hooo"
+tar :str = var + bar
+print(tar)
+print (tar[7])
+#looping in strings:
+for x in "divyansh":
+    print(x)
+#using function len() for getting touch with length of the string:
+c = 'student'
+print(len(c))
